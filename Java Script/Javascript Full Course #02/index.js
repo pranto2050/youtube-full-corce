6a5b1196107be803caf843be4260dfdb9361b16c
pranto-islam <prantoislamnt51@gmail.com>
2025-09-30 00:47:12 +0600

    Java script Jariable

--- Java Script/Javascript Full Course #02/index.js
+++ Java Script/Javascript Full Course #02/index.js
@@ -1,3 +1,28 @@
 
+// Basic Print
+// console.log("Hello World");
 
-console.log("Hello World")
+
+
+// Variable
+// 1. Let
+// 2. var
+// 3. Const
+
+
+// Let VS Var
+
+// String
+let name = "Md.Pranto Islam";
+// Integer
+let age = 30;
+// Update Value
+age = 80;
+
+console.log(name);
+console.log(age);
+
+
+
+
+
