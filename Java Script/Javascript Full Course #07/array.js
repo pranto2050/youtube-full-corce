e5a8bc40049a286b66a74007e240f57ca140f969
pranto-islam <prantoislamnt51@gmail.com>
2025-10-03 21:38:52 +0600

    Array Class

---
+++ Java Script/Javascript Full Course #07/array.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+
+// Arrays
+
+
+
+let array = [1, 2, 3, 4, 5];
+console.log(array); // Print array
+
+console.log(array.length); // Chack Array Length
+
+// Add Element in array in Last Position ->
+array.push(19,30,56,99);
+console.log(array.length);
+console.log(array);
+array.push('Santo','Desh');
+console.log(array);
+
+
+// Remove Array Elements
+array.pop();
+console.log(array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
