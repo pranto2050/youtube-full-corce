ffb8f8a0d98d1eea298ed5384c4d590a14ca24d8
pranto-islam <prantoislamnt51@gmail.com>
2025-10-04 12:46:25 +0600

    2D Array

--- Java Script/Javascript Full Course #07/array.js
+++ Java Script/Javascript Full Course #07/array.js
@@ -8,22 +8,127 @@
 
 
 
-let array = [1, 2, 3, 4, 5];
-console.log(array); // Print array
+//let array = [1, 2, 3, 4, 5];
+// console.log(array); // Print array
 
-console.log(array.length); // Chack Array Length
+// console.log(array.length); // Chack Array Length
 
 // Add Element in array in Last Position ->
-array.push(19,30,56,99);
-console.log(array.length);
-console.log(array);
-array.push('Santo','Desh');
-console.log(array);
+// array.push(19,30,56,99);
+// console.log(array.length);
+// console.log(array);
+// array.push('Santo','Desh');
+// console.log(array);
 
 
 // Remove Array Elements
-array.pop();
-console.log(array);
+// array.pop();
+// console.log(array);
+
+
+
+// For Of Loop
+
+// let arr =[10,20,30,40,50,60,70,80,90,100];
+// console.log(arr);
+
+// For of Lop -> Array
+// for(let num of arr){
+//     console.log(num);
+// };
+
+
+//  Now Array Referance
+
+// let new_arr = [10,40,48,99];
+// // Declear Other Variable
+// let array = new_arr;
+// console.log(array);
+// array.push(1,2,3,4,5);
+// // Here is array and new_arr Also change add all data in new_arr Because array Refarange in new_arr
+// console.log(array);
+// console.log(new_arr);
+
+
+
+
+
+// let new_arre = [10,40,48,99];
+//
+//
+// // Different ways to copy:
+// let arraya = [...new_arre];   // spread operator
+// // array.push(1,2,3,4,5);
+// console.log(new_arre);
+// arraya.push(13,23,34,45,56);
+// console.log(arraya);   // 👉 (13,23,34,45,56)
+// console.log(new_arre);
+
+
+
+// Add two array On a Single Array
+
+
+// let array = [1, 2, 3, 4, 5];
+// let array_two = ['Santo', 'Pranto', 'Rohan'];
+//
+// let total = array.concat(array_two);
+// console.log(total);
+
+
+
+
+
+// Join Oprator
+
+// let student_Name = ['Santo','Pranto','Rohan'];
+// let student_Email = ['Santo@gmail.com','Pranto@gmail.com','Rohan@gmail.com'];
+//
+// let full = student_Name.concat(student_Email);
+// console.log(full); //
+//
+// // More Way to Join One single array
+// let joine = student_Name.join('-')
+// console.log(joine); // Santo-Pranto-Rohan
+
+
+
+
+
+// Search Element In Array
+// let student_Name = ['Santo','Pranto','Rohan','Jibon','Rakib','yanto','mohan'];
+// console.log(student_Name);
+
+// Find Index Position
+
+// console.log(student_Name.indexOf('Rohan')); // find Index Position
+
+
+// // Odder Assending and Dassending
+// console.log(student_Name);
+// let assending = student_Name.sort();
+// console.log(assending);
+// let desingen = assending.reverse();
+// console.log(desingen);
+
+
+
+
+// let number_array = [10,101,40,21,84,11,79,93];
+// console.log(number_array);
+// console.log(number_array.sort());
+//
+// // Solution ->
+// // Assign Ding
+// console.log(number_array.sort((a,b)=>a-b));
+// // D Assignding Odder
+// console.log(number_array.sort((a,b)=>b-a));
+
+
+
+
+
+
 
 
 
