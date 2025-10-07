55319ea1a1f4181118b1ec4a4b383ee35e6d1ef0
pranto-islam <prantoislamnt51@gmail.com>
2025-10-07 19:59:22 +0600

    Nid Data

--- Java Script/Javascript Full Course #08/index.js
+++ Java Script/Javascript Full Course #08/index.js
@@ -4,6 +4,55 @@
 
 
 
+//  Object
+
+// Access Object Data using Two Way
+// 1. Dot Notation
+// 2. Backet Notation
+
+
+
+let user = {
+    name: `Pranto`,
+    age: 20,
+    email: "prantoislamnt51@gmail.com",
+    mobile: `01979728818`
+}
+// console.log(user.name) // Use Dot Notation
+// console.log(user[`name`]) // Use Backet Notation
+
+
+
+// Nid Card Information
+
+let users_data = {
+    name: `Pranto`,
+    age: 20,
+    email: "prantoislamnt51@gmail.com",
+    mobile: `01979728818`,
+    address: `Kushtia`,
+    id: 23303101
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
