3f03db67cca8e512a426eda80bd1b9c7a2603ba3
pranto-islam <prantoislamnt51@gmail.com>
2025-10-07 19:36:51 +0600

    Javascript Full Course #08

--- Java Script/Javascript Full Course #07/2D_array.js
+++ Java Script/Javascript Full Course #07/2D_array.js
@@ -9,7 +9,7 @@
 let arrays = [10,57,22,[38,69,34],89];
 
 console.log(arrays);
-
+console.log(arrays[3][2]); // Access 2D array Element
 
 
 
---
+++ Java Script/Javascript Full Course #07/arrays.js
@@ -0,0 +1,39 @@
+
+
+
+
+
+// Java scritp Array
+
+// What is an Array in JavaScript?
+
+let array = [1, 2, 3, 'Santo', 'Prappo', 4, 5];
+array.name = 'Samin';
+console.log(array);
+
+console.log(typeof array);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
