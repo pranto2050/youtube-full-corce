5436529511ce9f8ac6a40a3dd2bdf488839a989f
pranto-islam <prantoislamnt51@gmail.com>
2025-11-18 19:32:26 +0600

    Dom With Control

---
+++ Java Script/Javascript Full Course #13/index.js
@@ -0,0 +1,6 @@
+let element = window.document.getElementById("first");
+console.log(element);
+// Can Change inner Text On Website
+// element.innerText = "Santo";
+// Full Distructure
+console.dir(element);
