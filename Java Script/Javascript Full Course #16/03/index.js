0da7edb8e8568240493672e4bcb12dfbbc10456a
pranto-islam <prantoislamnt51@gmail.com>
2025-11-24 21:39:20 +0600

    Add Looping

--- Java Script/Javascript Full Course #16/03/index.js
+++ Java Script/Javascript Full Course #16/03/index.js
@@ -1,7 +1,69 @@
-let form = document.getElementById("myForm");
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault(); // stop the page from reloading
-    let input = document.getElementById("text_fild").value;
-    console.log(input);
-});
+
+
+// Full From
+const fromm = document.querySelector('form');
+// Input
+let boy = document.getElementById('boy');
+let girl = document.getElementById('girl');
+
+// Result Show:
+let result = document.querySelector("h1");
+// console.log(fromm);
+// Button Select
+fromm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let boyVale = boy.value;
+    let girlVale = girl.value;
+    // console.log(boyVale);
+    // console.log(girlVale);
+
+    let chack_boy =girlVale.length;
+    let girl_Chack = boyVale.length;
+
+    let resullt = Math.pow(chack_boy+girl_Chack,3) % 101;
+
+    console.log(girl_Chack);
+    console.log(chack_boy);
+
+    result.innerText = `Result : ${resullt} %`;
+    fromm.reset();
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Java Script/Javascript Full Course #16/04/index.js
+++ Java Script/Javascript Full Course #16/04/index.js
@@ -1,69 +1,25 @@
 
 
 
-// Full From
-const fromm = document.querySelector('form');
-// Input
-let boy = document.getElementById('boy');
-let girl = document.getElementById('girl');
 
-// Result Show:
-let result = document.querySelector("h1");
-// console.log(fromm);
-// Button Select
-fromm.addEventListener('submit', (e) => {
+
+let textFind = document.getElementById('text_fild');
+// console.log(textFind);
+
+let buttonClick = document.getElementById('button');
+
+let showText = document.getElementById("resulr");
+
+let showDiv = document.getElementById("show_texxt");
+
+buttonClick.addEventListener('click', function(e){
     e.preventDefault();
-    let boyVale = boy.value;
-    let girlVale = girl.value;
-    // console.log(boyVale);
-    // console.log(girlVale);
+    // console.log(showText.innerText);
 
-    let chack_boy =girlVale.length;
-    let girl_Chack = boyVale.length;
-
-    let resullt = Math.pow(chack_boy+girl_Chack,3) % 101;
-
-    console.log(girl_Chack);
-    console.log(chack_boy);
-
-    result.innerText = `Result : ${resullt} %`;
-    fromm.reset();
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    for (let i = 0; i <= 10; i++) {
+        let newHeading = document.createElement("h1");
+        newHeading.innerText = textFind.value;
+        showDiv.appendChild(newHeading);
+    }
+});
 
