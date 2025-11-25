7c839da24235a6a4ad7f65b3d97b2b5a92ade3b3
pranto-islam <prantoislamnt51@gmail.com>
2025-11-25 23:14:10 +0600

    Add count Down

--- Java Script/Javascript Full Course #16/07/index.js
+++ Java Script/Javascript Full Course #16/07/index.js
@@ -3,3 +3,19 @@
 
 
 
+
+// const fild_find = document.getElementById("year");
+
+const button = document.getElementById("btn");
+
+// button.addEventListener((e)=>{
+//     e.preventDefault()
+//     console.log("hellow");
+// })
+
+console.log("dfs");
+
+
+// console.log("hello world");
+
+
