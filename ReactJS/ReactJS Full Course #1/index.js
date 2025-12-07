1f88022c1da63b2a3fbd98507673ff36cf917667
pranto-islam <prantoislamnt51@gmail.com>
2025-12-07 17:57:20 +0600

    Add New Render

--- ReactJS/ReactJS Full Course #1/index.js
+++ ReactJS/ReactJS Full Course #1/index.js
@@ -1,1 +1,33 @@
- 
+
+
+
+
+
+
+function createElement (tag,artubete,cildren){
+    const elleent = document.createElement(tag);
+    elleent.textContent = children;
+    for(const key in artubete){
+        elleent[key] = artubete[key];
+    }
+
+    return elleent;
+}
+
+
+
+
+const element = document.createElement('h1', {className: 'heading',id: 'thisIsHeading',style:{backgroundColor: 'red',color: 'Blue'}},'I am Pranto');
+
+const root = document.getElementById('root');
+
+
+
+const rea = "hi";
+
+
+
+
+
+
+
