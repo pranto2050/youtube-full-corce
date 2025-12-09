44514d3480e28c21a4e08a6a1d05eb70ec6def8d
pranto-islam <prantoislamnt51@gmail.com>
2025-12-09 22:37:56 +0600

    Add Babel link

--- ReactJS/ReactJS Full Course #2/index.js
+++ ReactJS/ReactJS Full Course #2/index.js
@@ -1,1 +1,26 @@
- 
+
+
+// This Is too Hard
+// const element = React.createElement('h1', null, 'Hello World');
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+//
+// root.render(element);
+
+
+
+// Easy way to use and use Babling----> JSX= javasctipt XML it's Like HTML
+
+
+const element = <h1>Hi i am Pranto </h1>;
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(element);
+
+
+
+
+
+
+
